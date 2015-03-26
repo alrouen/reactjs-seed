0a8093f702032c75dc708216cfc7d052236048da
AlexJozwicki <contact@alexandrejozwicki.com>
2015-03-27 00:45:19 +0100

    Revert "Clean up for a base start"
    This reverts commit 77d3a175fb16268a3e41a115078001196bedf269.

--- src/js/app.js
+++ src/js/app.js
@@ -5,15 +5,19 @@ require( 'whatwg-fetch' );
 var React   = require('react');
 var Router  = require('react-router');
 var Home    = require('./home');
-var BasicReactSample = require( './BasicReactSample' );
+var { Page1, Page2, Page3 } = require( "./pages" );
+
 
 /**
  * The routes of your application
  */
 const routes = (
     <Router.Route path="/" handler={Home}>
-        <Router.DefaultRoute handler={BasicReactSample} />
-        <Router.NotFoundRoute handler={BasicReactSample}/>
+        <Router.DefaultRoute handler={Page1} />
+        <Router.Route name="page1" path="/page1" addHandlerKey={true} handler={Page1}/>
+        <Router.Route name="page2" path="/page2" addHandlerKey={true} handler={Page2}/>
+        <Router.Route name="page3" path="/page3" addHandlerKey={true} handler={Page3}/>
+        <Router.NotFoundRoute handler={Page1}/>
     </Router.Route>
 );
 
---
+++ src/js/pages/index.js
@@ -0,0 +1,10 @@
+/**
+ * pages used throughout the application.
+ *
+ * @package page
+ */
+module.exports = {
+    Page1: require( "./Page1" ),
+    Page2: require( "./Page2" ),
+    Page3: require( "./Page3" )
+};
